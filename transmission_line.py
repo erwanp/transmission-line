3f9509a4a6135351d397db1be1e4a58046281efd
Erwan Pannier <erwan.pannier@gmail.com>
2016-07-21 13:35:23 +0200

    fixed animation
    now precomputes calculations, then display

--- transmission_line.py
+++ transmission_line.py
@@ -11,6 +11,14 @@ Updated Erwan Pannier 30/11/15:
 
 """
 
+import matplotlib
+#matplotlib.use("TkAgg")
+
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# implement the default mpl key bindings
+#from matplotlib.backend_bases import key_press_handler
+#from matplotlib.figure import Figure
+
 import numpy as np
 from numpy import zeros, ceil, sqrt
 import matplotlib.pyplot as plt
@@ -18,7 +26,58 @@ from matplotlib import animation
 from matplotlib import gridspec
 from numba import jit
 
-class line():
+
+#    @jit
+class Pulser():
+    '''
+    Pulser: force voltage
+    
+    Input
+    -----------
+    
+    - Z                   # pulser impedance
+    - Voff
+    - Von
+    - tRise
+    - tFall
+    - tPeriod             # between two pulses (s)
+    - tOn                 # full amplitude (s)
+    
+    '''
+    
+    def __init__(self,Z,Von, Voff, tRise, tOn, tFall, tPeriod):
+        
+        self.Z = Z
+        self.Voff = Voff
+        self.Von = Von
+        self.tRise = tRise
+        self.tFall = tFall
+        self.tPeriod = tPeriod
+        self.tOn = tOn
+        
+    def V(self,t):
+        '''
+        Calculates the output voltage of the pulse source at instant t
+        '''
+        Voff, Von, tRise, tFall, tPeriod, tOn = (
+                self.Voff, self.Von, self.tRise, self.tFall, self.tPeriod, self.tOn)
+                
+        t = t % tPeriod
+        Vs = self.Voff
+        if (t < tRise):
+            Vs = (Von - Voff) * t / tRise + Voff
+        elif (t < tRise + tOn):
+            Vs = Von
+        elif (t < tRise + tOn + tFall):
+            Vs = (Voff - Von) * (t-tRise-tOn) / tFall + Von
+        return Vs
+ 
+class Load():
+    
+    def __init__(self,Z):
+        self.Z = Z
+
+class Line():
  
     '''
     A model for a transmission line. Original code from:
@@ -30,15 +89,10 @@ class line():
     - d = 2               # line length (m)
     - eps = 2.25          # dielectric permittivity (2.25 typical for polyethylene)
      
-    - R1                  # pulser impedance
-    - R2                  # Load impedance
+    - inp:                # Input (ex: pulser)
+    - out:                # Output (ex: load)
+    
     - dt                  # time resolution. lower = better but more time consuming
-    - Voff
-    - Von
-    - tRise
-    - tFall
-    - tPeriod             # between two pulses (s)
-    - tOn                 # full amplitude (s)
      
     # Scope
     - xscope              # % of total cable length where to place the scope [0-1]
@@ -49,8 +103,8 @@ class line():
     
     '''    
        
-    def __init__(self,Z,R1,R2,d,tRise,tFall,tPeriod,tOn,eps=2.25,Von=1,Voff=0,
-                 xscope=1,dt=1e-11,elems=256,maxSteps=1000,interval=20):
+    def __init__(self,Z,Inp,Out,d,eps=2.25,
+                 xscope=1,elems=256,interval=20):
         
         # Model parameters    
         v = 2.998e8/sqrt(eps)   # signal speed (m/s)
@@ -60,7 +114,16 @@ class line():
         
         # Scope    
         self.iscope = int(ceil(xscope*elems))
-        self.tscope, self.Vscope = ([], [])
+        
+        
+        self.elems = elems
+        self.interval = interval        
+        
+        # Input / output objects
+        self.Inp = Inp
+        self.Out = Out
+        R1 = Inp.Z
+        R2 = Out.Z
         
         # %% build simulation
         G1 = 1 / R1
@@ -78,14 +141,52 @@ class line():
         for i in range(1, elems):
             diags[i] = 2 * GL + GC - GL * GL / diags[i - 1]
         diags[elems] = GC + GL + G2 - GL * GL / diags[elems - 1]
-         
-        self.V = zeros(elems + 1)
-        self.I = zeros(2 * elems)
+        
+        # storage dictionary for voltage / intensity
+        self.V = {}
+        self.I = {}
+        
+        # %% Start
+        self.x = x = np.linspace(0,d,elems+1)  # line
+        self.xindex = np.arange(len(x))
+   
+        # Adjust interval to change the animation speed
+#        params = [G1, G2, GC, GL,tRise,tFall,tPeriod,tOn,eps,Von,Voff,xscope,
+#                 dt,elems,diags,x]
+        params = [G1, G2, GC, GL, elems, diags,x]
+        self.params = params
+        
+    def solve(self,tmax=20e-9,dt=1e-11):
+        ''' do all calculations '''
+        
+        tarr = np.arange(0,tmax,dt)
+        self.t = tarr
+        
+        self.V[0] = V = zeros(self.elems + 1)
+        self.I[0] = I = zeros(2 * self.elems)
+        
+        for t in tarr:
+            # source voltage
+            Vs = self.Inp.V(t)
+            
+            # line voltage
+            V, I = self.simStep(V, I, Vs, dt)
+            
+            self.V[t], self.I[t] = V, I
+            
+    def get_V(self,t,xindex):
+        ''' Assumes already calculated '''
+        
+        return self.V[t][xindex]
+    
+    def plot(self):
         
         # Plot everything
-        x = np.linspace(0,d,elems+1)  # line
+
+        x = self.x
+        
         plt.close()
-        fig = plt.figure()
+        self.fig = plt.figure()
         gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1],hspace=0.5)
         ax = plt.subplot(gs[0])
         ax.set_xlim((0,d))
@@ -93,7 +194,7 @@ class line():
         ax.set_ylabel('Voltage (V)')
         ax.set_xlabel('Line (m)')
         ax.set_title('Line ${0:.0f}\Omega$, Load ${1:.0f}\Omega$, Pulse ${2:.0f}ns$ with ${3:.0f}ns$ rise time'.format(
-                                Z,R1,tOn*1e9,tRise*1e9))
+                                Z,self.Inp.Z,self.Inp.tOn*1e9,self.Inp.tRise*1e9))
         ax.plot(x,np.zeros_like(x),':k')
         ax.plot([xscope*d]*2,ax.get_ylim(),'-r',linewidth=2,alpha=0.3)
         self.line, = ax.plot([], [], lw=2)
@@ -109,45 +210,29 @@ class line():
         ax2.grid(True)
         self.lscope, = ax2.plot([], [], lw=2)
         
-        # %% Start
-        self.step = 0
-        
-        # Adjust interval to change the animation speed
-        args = [G1, G2, GC, GL,tRise,tFall,tPeriod,tOn,eps,Von,Voff,xscope,
-                 dt,elems,diags,x]
-        self.args = args
-        animation.FuncAnimation(fig, self.animate, fargs=[args,],init_func=self.init,
-               frames=maxSteps, interval=interval, blit=True)
-        
-        plt.show()                    
+    def start(self,nframes=1000):
+        ''' note: animation has to create a self.anim ?'''
+
+        self.anim = animation.FuncAnimation(self.fig, self.animate, init_func=self.init,
+               frames=nframes, interval=self.interval)
+               #, blit=True,repeat=True,               repeat_delay=500)
+#                           
+#        self.anim = animation.FuncAnimation(self.fig, self.animate,
+#            frames=nframes, interval=50, blit=True,repeat=True,
+#            repeat_delay=500)
             
-    @jit
-    def pulse(self,t, Voff, Von, tRise, tFall, tPeriod, tOn):
+#    @jit
+    def simStep(self, V, I, Vs, dt):
         '''
-        Calculates the output voltage of a pulse source
-        '''
-        t = t % tPeriod
-        Vs = Voff
-        if (t < tRise):
-            Vs = (Von - Voff) * t / tRise + Voff
-        elif (t < tRise + tOn):
-            Vs = Von
-        elif (t < tRise + tOn + tFall):
-            Vs = (Voff - Von) * (t-tRise-tOn) / tFall + Von
-        return Vs
- 
-    @jit
-    def simStep(self,G1, G2, GC, GL, dt, elems, Voff, Von, tRise, tFall, 
-                tPeriod, tOn, diags, V, I):
-        '''
-        Simulates one time step
+        Simulates one time step starting from V, I with timestep dt 
         
         Note: performance test:
         - without @jit: 100 loops, best of 3: 1.97 ms per loop
         - with @jit: 10000 loops, best of 3: 16.1 µs per loop
         '''
-        # source voltage
-        Vs = self.pulse(self.step*dt, Voff, Von, tRise, tFall, tPeriod, tOn)
+
+        (G1, G2, GC, GL, elems, diags,x) = self.params
+                
         # calculate norton currents for caps/inductors
         INort = zeros(2 * elems)
         for i in range(0, elems):
@@ -196,35 +281,30 @@ class line():
  
 
     def init(self):    
+
         self.ttl.set_text('')
         self.line.set_data([], [])
         self.lscope.set_data([], [])
-        return self.line, self.ttl, self.lscope
+        self.tscope, self.Vscope = ([], [])
+        return self.line, self.lscope, self.ttl
                     
     # animation function.  This is called sequentially
-    def animate(self,i,args):
+    def animate(self,i):
     #    global G1, G2, GC, GL, dt, step, elems, Voff, Von, tRisetFall, tPeriod
     #    global tOn, diags, V, I,x, ax, tscope, Vscope, iscope, lscope
     
-        [G1, G2, GC, GL,tRise,tFall,tPeriod,tOn,eps,Von,Voff,xscope,
-                 dt,elems,diags,x] = args    
+        t = self.t[i]
         
-        self.V, self.I = self.simStep(G1, G2, GC, GL, dt, elems, Voff, Von, tRise, 
-                           tFall, tPeriod, tOn, diags,self.V,self.I)
-        
-#        print('V',self.V)
-#        print('step',self.step)
-        self.line.set_data(x,self.V)    
-        self.ttl.set_text('{0:.1f}ns'.format(self.step*dt*1e9))
+        self.line.set_data(self.x,self.get_V(t,self.xindex))    
+        self.ttl.set_text('{0:.1f}ns'.format(t*1e9))
         
         # scope
-        self.tscope.append(-self.step*dt*1e9)
-        self.Vscope = [self.V[self.iscope]] + self.Vscope
+        self.tscope.append(-t*1e9)
+        self.Vscope = [self.V[t][self.iscope]] + self.Vscope
         self.lscope.set_data(self.tscope,self.Vscope)
         
-        self.step += 1
         
-        return self.line, self.ttl, self.lscope
+        return self.line, self.lscope, self.ttl
     
 
 if __name__ == '__main__':
@@ -233,15 +313,28 @@ if __name__ == '__main__':
     Z = 50              # equivalent line impedance (Ohm)
     d = 2               # line length (m)
      
-    R1 = 50             # pulser impedance
-    R2 = 1e9            # Load impedance
-    dt = 5e-11          # time resolution. lower = better but more time consuming
+    Zin = 300             # pulser impedance
+    Zout = 1e-9            # Load impedance
+    tmax = 100e-9
+    dt = 1e-10          # time resolution. lower = better but more time consuming
     tRise = 0.1e-9 #1e-9
     tFall = tRise
-    tPeriod = 50e-9 #1000e-9
+    tPeriod = 5000e-9 #1000e-9
     tOn = 5e-9 # tPeriod / 2 - tRise
+    Von = 4
+    Voff = 0
      
     # Scope
-    xscope = 0.9    # % of total cable length where to place the scope
+    xscope = 0.59   # % of total cable length where to place the scope
     
-    tl=line(Z,R1,R2,d,tRise,tFall,tPeriod,tOn,dt=dt,interval=0,xscope=xscope)
+    fid = Pulser(Zin,Von,Voff,tRise,tOn,tFall,tPeriod)
+    load = Load(Zout)
+    tl=Line(Z,fid,load,d,eps=2.25,xscope=xscope,interval=1)
+    
+    tl.solve(tmax,dt)
+    
+    tl.plot()
+    nframes = int(tmax//dt)
+    tl.start(nframes=nframes)
+    
+    plt.show() 
